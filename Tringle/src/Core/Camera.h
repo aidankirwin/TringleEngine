be453682ec3ecb36df0f2c7ef7b393fd54f01a3d
akirwin <akirwin@uwaterloo.ca>
2023-11-25 20:57:51 -0500

    Started shader class.

--- Tringle/src/Core/Camera.h
+++ Tringle/src/Core/Camera.h
@@ -9,25 +9,19 @@ public:
 	glm::mat4 GetViewTransform();
 	glm::mat4 GetProjectionMatrix();
 
-	enum Projection
-	{
-		ORTHOGRAPHIC,
-		PERSPECTIVE
-	};
 private:
 	Projection mView;
 	glm::vec3 mPosition;
 	float mAspectRatio;
 
+	// TODO
+
 	glm::mat4 GetOrthoProjMat();
 	glm::mat4 GetPerspProjMat();
 };
 
-struct CameraProps
+enum Projection
 {
-	float Up;
-	float Front;
-	float Yaw;
-	float Pitch;
-	float FOV;
+	ORTHOGRAPHIC,
+	PERSPECTIVE
 };
--- Tringle/src/Graphics/Shader.h
+++ Tringle/src/Graphics/Shader.h
@@ -1,9 +1,23 @@
 #pragma once
 
 #include <vector>
+#include <map>
+#include <string>
 #include "glm.hpp"
 
 class Shader
 {
+public:
+	Shader(std::string vertPath, std::string fragPath);
+	Shader Use();
+	int GetAttribLocation(std::string attribName);
 
+
+	GLuint Handle;
+
+private:
+	void CompileShader(int shader);
+	void LinkProgram(int program);
+
+	std::map<std::string, int> mUniformLocations;
 };
